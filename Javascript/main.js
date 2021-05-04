b0b971d388ebed28d6a73a71b280a4b1d2301c63
Ricky Greig <78995684+Rickygreig@users.noreply.github.com>
2021-05-04 14:10:08 -0400

    Add files via upload

---
+++ Javascript/main.js
@@ -0,0 +1,25 @@
+
+
+/*// data.json
+{
+
+    "jobs": [
+        ["Date", "Title", "Company"],
+        ["", "", ""],
+        ["", "", ""]
+    ]
+}
+
+
+// main.js
+$.ajax("/data.json", function(data) {
+    for(row in data.jobs) {
+        createElement("tr");
+    }
+})
+
+$.click("#dragonEye", function() {
+    /// literally anything 
+
+})
+*/
