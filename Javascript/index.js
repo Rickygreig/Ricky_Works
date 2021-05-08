14ae96cfb4fe8e6bf8aeabc778018a569b4cea26
Ricky Greig <78995684+Rickygreig@users.noreply.github.com>
2021-05-07 22:02:32 -0400

    Add files via upload

---
+++ Javascript/index.js
@@ -0,0 +1,25 @@
+let interestRate = 0.3;
+console.log(interestRate)
+/*// data.json
+{
+
+    "jobs": [
+        ["Date", "Title", "Company"],
+        ["", "", ""],
+        ["", "", ""]
+    ]
+}
+
+
+// main.js
+$.ajax("/data.json", function(data) {
+    for(row in data.jobs)
+        createElement("tr");
+    }
+})
+
+$.click("#dragonEye", function() {
+    /// literally anything 
+
+})
+*/
